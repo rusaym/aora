28091beebac0c58e0a97d2b4b7df4a920e758e6d
Ruslan Mavletkulov <mavletkulov@rusapr.ru>
2024-05-08 00:57:00 +0300

    folders structure

--- app/index.jsx
+++ app/index.jsx
@@ -5,10 +5,10 @@ import { Text, View } from 'react-native'
 export default function App() {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-3xl'>Aora!</Text>
+      <Text className='text-3xl font-pblack'>Aora!</Text>
       <StatusBar style='auto' />
-      <Link href='/profile' style={{ color: 'blue' }}>
-        Go to Profile
+      <Link href='/home' style={{ color: 'blue' }}>
+        Go to Home
       </Link>
     </View>
   )

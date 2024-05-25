7d5d59b9e67fed5832b52d07ef2e9ffe8fe7cd41
Ruslan Mavletkulov <mavletkulov@rusapr.ru>
2024-05-25 11:23:53 +0300

    updates

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../components/FormField'
 import CustomButton from '../components/CustomButton'
 import { Link } from 'expo-router'
+import { createUser } from '../../lib/appwrite'
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -13,9 +14,22 @@ const SignUp = () => {
     password: '',
   })
 
-  const [isSubmitting, setIsSubmittign] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {}
+  const submit = async () => {
+    if (!form.username || !form.email || form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+    setIsSubmitting(true)
+
+    try {
+      const result = await createUser(form.email, form.password, form.username)
+    } catch (error) {
+      Alert.alert('Error', error.message)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
 
   return (
     <SafeAreaView className='bg-primary h-full'>
